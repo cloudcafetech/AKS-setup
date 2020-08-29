a130d6500682a7bf903764d62a6debe5154f01f8
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-29 16:22:46 +0530

    Update windows-exporter-setup.bat

--- windows-exporter-setup.bat
+++ windows-exporter-setup.bat
@@ -2,5 +2,3 @@ mkdir -p C:\aks
 cd C:\aks
 curl -LO https://github.com/prometheus-community/windows_exporter/releases/download/v0.13.0/windows_exporter-0.13.0-amd64.msi
 msiexec /i C:\aks\windows_exporter-0.13.0-amd64.msi /quiet /q /norestart /log c:\aks\install.log ENABLED_COLLECTORS=os,iis,cpu,system,container,logical_disk,net,cs,memory,terminal_services,tcp LISTEN_PORT=9100
-curl -LO https://github.com/petrkle/vim-msi/releases/download/v8.1.55/vim-8.1.55.msi
-msiexec /i C:\aks\vim-8.1.55.msi /quiet /q /norestart /log c:\aks\installvim.log
