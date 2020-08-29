909665cc879f9e0b18cad46e65fc11c8486c6c62
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-29 15:53:28 +0530

    Update and rename windows-vim-setup.bat to windows-vim-setup.ps1

--- windows-vim-setup.bat
+++
@@ -1,4 +0,0 @@
-mkdir -p C:\tools
-cd C:\tools
-curl -LO https://github.com/petrkle/vim-msi/releases/download/v8.1.55/vim-8.1.55.msi
-msiexec /i C:\tools\vim-8.1.55.msi /quiet /q /norestart /log c:\aks\installvim.log
