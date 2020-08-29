21f3139b4001d26c62fcae2f016f30a09e5a877e
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-29 10:36:12 +0530

    Update winexporter-setup.sh

--- winexporter-setup.sh
+++ winexporter-setup.sh
@@ -3,6 +3,7 @@
 
 CLUSTER=prod-aks-win
 WINUSER=adminprod
+SSHKEY=ssh-key-$CLUSTER
 
 echo "Deploying AKS SSH POD on Cluster"
 kubectl create -f https://raw.githubusercontent.com/cloudcafetech/AKS-setup/master/aks-ssh.yaml
@@ -11,21 +12,21 @@ echo "Waiting for SSH POD ready .."
 while [[ $(kubectl get pods aks-ssh -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do printf '.'; sleep 2; done
 
 echo "Copy ssh key to SSH POD"
-kubectl cp ssh-key-$CLUSTER $(kubectl get pod aks-ssh | awk '{print $1}' | grep -v NAME):/ssh-key-$CLUSTER
+kubectl cp $SSHKEY $(kubectl get pod aks-ssh | awk '{print $1}' | grep -v NAME):/$SSHKEY
 
 for winhost in $(kubectl get nodes -o wide | grep Windows | awk '{print $6}'); do
   echo "Deploying Host Exporter on Windows Nodes"
-  kubectl exec -it aks-ssh -- ssh -o 'StrictHostKeyChecking no' -i ssh-key-$CLUSTER $WINUSER@$winhost "curl -LO https://raw.githubusercontent.com/cloudcafetech/AKS-setup/master/windows-exporter-setup.bat && windows-exporter-setup.bat"
+  kubectl exec -it aks-ssh -- ssh -o 'StrictHostKeyChecking no' -i $SSHKEY $WINUSER@$winhost "curl -LO https://raw.githubusercontent.com/cloudcafetech/AKS-setup/master/windows-exporter-setup.bat && windows-exporter-setup.bat"
 done
 
 echo "Deleting SSH POD"
 kubectl delete pod aks-ssh
 
 # Cluster linux host login
-#kubectl exec -it aks-ssh -- ssh -o 'StrictHostKeyChecking no' -i ssh-key-$CLUSTER azureuser@<LINUX HOST IP>
+#kubectl exec -it aks-ssh -- ssh -o 'StrictHostKeyChecking no' -i $SSHKEY azureuser@<LINUX HOST IP>
 
 # Cluster Windows host login
-#kubectl exec -it aks-ssh -- ssh -o 'StrictHostKeyChecking no' -i ssh-key-$CLUSTER $WINUSER@<Windows HOST IP>
+#kubectl exec -it aks-ssh -- ssh -o 'StrictHostKeyChecking no' -i $SSHKEY $WINUSER@<Windows HOST IP>
 
 # Exit container cleanup command
 #docker rm `docker ps -a | grep -v CONTAINER | grep Exited | awk '{print $1}'`
