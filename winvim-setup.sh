1d722a54e400fe64158e031b9a8db61d8113c930
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2020-08-29 15:40:05 +0530

    Create winvim-setup.sh

---
+++ winvim-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Windows vi setup script on AKS Windows Host
+
+CLUSTER=prod-aks-win
+WINUSER=adminprod
+SSHKEY=ssh-key-$CLUSTER
+
+echo "Deploying AKS SSH POD on Cluster"
+kubectl create -f https://raw.githubusercontent.com/cloudcafetech/AKS-setup/master/aks-ssh.yaml
+
+echo "Waiting for SSH POD ready .."
+while [[ $(kubectl get pods aks-ssh -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do printf '.'; sleep 2; done
+
+echo "Copy ssh key to SSH POD"
+kubectl cp $SSHKEY $(kubectl get pod aks-ssh | awk '{print $1}' | grep -v NAME):/$SSHKEY
+
+for winhost in $(kubectl get nodes -o wide | grep Windows | awk '{print $6}'); do
+  echo "Deploying Host Exporter on Windows Nodes"
+  kubectl exec -it aks-ssh -- ssh -o 'StrictHostKeyChecking no' -i $SSHKEY $WINUSER@$winhost "curl -LO https://raw.githubusercontent.com/cloudcafetech/AKS-setup/master/windows-vi-setup.bat && windows-vi-setup.bat"
+done
+
+echo "Deleting SSH POD"
+kubectl delete pod aks-ssh
